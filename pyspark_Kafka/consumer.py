9e8b335000b3feb284610e494ef6e39100301e3b
Gadaite <rangeyang22@gmail.com>
2022-04-01 09:53:52 +0800

    Gadaite

---
+++ pyspark_Kafka/consumer.py
@@ -0,0 +1,20 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+sc = SparkContext(appName="streamingkafkaconsumer")
+sc.setLogLevel("WARN") #设置日志级别
+ssc = StreamingContext(sc, 5) # 计算时间间隔
+brokers='192.168.1.10:9092' # IP：端口
+topic = 'sparkapp' #kafka主题
+# 使用streaming使用直连模式消费kafka
+kafka_streaming_rdd = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+lines_rdd = kafka_streaming_rdd.map(lambda x: x[1])
+counts = lines_rdd.flatMap(lambda line: line.split(" ")) \
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a+b)
+# 将workcount结果打印到当前shell
+counts.pprint()
+ssc.start()
+ssc.awaitTermination()
+property = property()
--- pyspark_Kafka/producer.py
+++ pyspark_Kafka/producer.py
@@ -1,21 +1,12 @@
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-
-sc = SparkContext(appName="streamingkafka")
-sc.setLogLevel("WARN") #设置日志级别
-ssc = StreamingContext(sc, 5) # 计算时间间隔
-brokers='192.168.1.10:9092' # IP：端口
-topic = 'sparkapp' #kafka主题
-# 使用streaming使用直连模式消费kafka
-kafka_streaming_rdd = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-lines_rdd = kafka_streaming_rdd.map(lambda x: x[1])
-counts = lines_rdd.flatMap(lambda line: line.split(" ")) \
-    .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a+b)
-# 将workcount结果打印到当前shell
-counts.pprint()
-ssc.start()
-ssc.awaitTermination()
-
-property = property()
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+producer =  KafkaProducer(bootstrap_servers='192.168.1.10:9092')
+while True:
+    msg = input("input the msg:")
+    future = producer.send("sparkapp", msg.encode())
+    # def send(self, topic, value=None, key=None, headers=None, partition=None, timestamp_ms=None):
+    try:
+        record = future.get(timeout=10)
+        # print(record)
+    except KafkaError as e:
+        print(e)
