080fffbec8d7d1b0cc3ebf52888fbdbf3ebfa480
Gadaite <rangeyang22@gmail.com>
2022-04-03 16:38:36 +0800

    Gadaite

--- pydatabases/SSqlalchemy.py
+++ pydatabases/SSqlalchemy.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
 engine = create_engine('mysql+pymysql://root:LYP809834049@192.168.1.10:3306/Gadaite')
 print(engine)
 sql = 'select * from seeds_dataset'
